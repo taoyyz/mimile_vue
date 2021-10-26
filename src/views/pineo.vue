22c8c15a53df74f8432cc9e3296d203164f7cec2
Yamke <1901349399@QQ.XOM>
2021-10-26 16:58:41 +0800

    合作商家

---
+++ src/views/pineo.vue
@@ -0,0 +1,21 @@
+<template>
+  <!--展示合作网站 -->
+  <table id = 'footer'>
+    <tr>
+      <th>
+        <a href="App.vue">本家</a>
+      </th>
+      <th>
+        <a href="https://www.taobao.com/">淘宝</a>
+      </th>
+      <th>
+        <a href="https://www.jd.com/">京东</a>
+      </th>
+    </tr>
+  </table>
+</template>
+<script>
+</script>
+<style scoped src="../assets/css/hadi.css">
+
+</style>
