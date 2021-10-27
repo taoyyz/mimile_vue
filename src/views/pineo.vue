576b86759e4185dac0ba270f729eba55fa90d6b9
Yamke <1901349399@QQ.XOM>
2021-10-27 18:56:50 +0800

    修改

--- src/views/pineo.vue
+++ src/views/pineo.vue
@@ -3,18 +3,20 @@
   <table id = 'footer'>
     <tr>
       <th>
-        <a href="App.vue">本家</a>
+<!--  因为原生的a标签不能使用便采用 Router-link进行页面跳转-->
+        <router-link to="/App">本家</router-link>>
       </th>
       <th>
-        <a href="https://www.taobao.com/">淘宝</a>
+        <router-link to="https://www.taobao.com/">淘宝</router-link>
       </th>
       <th>
-        <a href="https://www.jd.com/">京东</a>
+        <router-link to="https://www.jd.com/">京东</router-link>
       </th>
     </tr>
   </table>
 </template>
 <script>
+
 </script>
 <style scoped src="../assets/css/hadi.css">
 
